2e76da208bf9bc827e7567d6a9d4d47a9a384b8a
anamariabrinza <brinzaaanamaria@gmail.com>
2017-06-11 19:14:37 +0300

    Some modifications

--- voting/election/forms.py
+++ voting/election/forms.py
@@ -11,8 +11,20 @@ class ElectionForm(forms.ModelForm):
 # asta cumva trebuie de inclus pe pagina Create Election
 # Adaugam optiunile de vot, 2 sau mai multe
 # optionName from ElectionChoices
+
 class ChoiceForm(forms.ModelForm):
 
     class Meta:
         model = ElectionChoices
-        fields = ['optionName']
+        exclude = ('election_id',)
+
+    """
+    Form for election choices
+   
+
+    option = forms.CharField(
+                    max_length=100,
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'Election choice/option',
+                    }),
+                    required=False)  """
--- voting/election/views.py
+++ voting/election/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import reverse
+from django.forms.formsets import formset_factory
 from .forms import ElectionForm, ChoiceForm
-from .models import Election
+from .models import Election, ElectionChoices
 from django.views import View
 
 # Create your views here.
@@ -31,15 +32,36 @@ class ElectionDetail(View):
 # there can be 2 ore even more options per election
 class CreateElection(View): # creating a new election only by administration and EC
 
+    # Create the formset, specifying the form and formset we want to use.
+
     def get(self, request):
+
         election_form = ElectionForm()
-        return render(request, 'elections/create_election.html', {'election_form': election_form})
+        choice_form = ChoiceForm()
+        context = {
+            'election_form': election_form,
+            'choice_form': choice_form,
+        }
+        return render(request, 'elections/create_election.html', context)
 
     def post(self, request):
-        election_form = ElectionForm(request.POST)
-        if election_form.is_valid():
-            election_form.save()
+        #ChoiceFormSet = formset_factory(ChoiceForm)
+
+        election_form = ElectionForm(request.POST, instance=Election())
+        choice_form = [ChoiceForm(request.POST, prefix=str(x), instance=ElectionChoices())for x in range(0, 3)]
+
+        if election_form.is_valid() and all([cf.is_valid() for cf in choice_form]):
+            new_election = election_form.save()
+
+            for cf in choice_form:
+                new_choice = cf.save(commit=False)
+                new_choice.election_id = new_election
+                new_choice.save()
+
             return redirect(reverse('election:election-name'))
         else:
-            return render(request, 'elections/create_election.html', {'election_form': election_form})
+            return render(request, 'elections/create_election.html', {
+                'election_form': election_form,
+                'choice_formset': choice_form,
+            })
 
