f1e41d3c4370a36056def3ff827f4cb9462f603a
anamariabrinza <brinzaaanamaria@gmail.com>
2017-06-19 21:41:20 +0300

    modifications and features added to django admin (adding/editing elections)

--- voting/election/admin.py
+++ voting/election/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Election, ElectionChoices
 # Register your models here.
 
-admin.site.register(Election)
-admin.site.register(ElectionChoices)
+
+class ElectionChoicesInline(admin.TabularInline):
+    model = ElectionChoices
+    extra = 2
+
+class ElectionAdmin(admin.ModelAdmin):
+
+
+    inlines = [ElectionChoicesInline]
+    list_display = ('name', 'startDate')
+    list_filter = ['startDate']
+
+    search_fields = ['name']
+
+admin.site.register(Election, ElectionAdmin)
+
