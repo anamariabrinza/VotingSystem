fef0bac66dc6e545dcd70e27c8205a5b341e4fe3
anamariabrinza <brinzaaanamaria@gmail.com>
2017-06-21 01:54:26 +0300

    bootstrap added

--- voting/election/views.py
+++ voting/election/views.py
@@ -45,12 +45,20 @@ class ElectionDetail(LoginRequiredMixin, View):
         election_id = Election.objects.get(pk=pk)
         return render(request, 'election/election_detail.html', {'election':election_id})
 
+    def post(self, request):
+
+        if request.POST:
+            return redirect('/success/')
+        else:
+            return redirect('/success/')
+    '''
     def vote(self, request, pk):
         e = get_object_or_404(Election, pk=pk)
         try:
             selected_choice = e.electionchoices_set.get(pk=request.POST['choice'])
         except (KeyError, ElectionChoices.DoesNotExist):
             # Redisplay the poll voting form.
+
             return render_to_response('election/election_detail.html', {
                 'election': e,
                 'error_message': "You didn't select a choice.",
@@ -61,10 +69,14 @@ class ElectionDetail(LoginRequiredMixin, View):
             # Always return an HttpResponseRedirect after successfully dealing
             # with POST data. This prevents data from being posted twice if a
             # user hits the Back button.
-            return HttpResponseRedirect(reverse('election.results', args=(e.id,)))
+            return HttpResponseRedirect(reverse('results/', args=(e.id,)))
+        '''
 
 
 class CreateElection(LoginRequiredMixin, CreateView): # creating a new election only by administration and EC
+    def get(self,request):
+        return redirect('/admin/election/election/add/')
+    '''
     model = Election
     fields = ['name', 'description', 'startDate', 'endDate', 'status', 'group']
     success_url = reverse_lazy('election:election-name')
@@ -87,6 +99,7 @@ class CreateElection(LoginRequiredMixin, CreateView): # creating a new election 
                 electionchoices.instance = self.object
                 electionchoices.save()
         return super(CreateElection, self).form_valid(form)
+        '''
 
 class Results(View):
 
--- voting/voting/settings.py
+++ voting/voting/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'election',
+    'bootstrap3',
     'user',
     'rules'
+
 ]
 
 MIDDLEWARE = [
